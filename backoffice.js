74271078fb1639104d1590e92d8ee72772e25af3
Dickson Wong <dicksonwong@gmail.com>
2024-12-16 13:31:38 -0800

    Sort store inventory by price

--- backoffice.js
+++ backoffice.js
@@ -1,14 +1,40 @@
 function handleBackoffice() {
+    sortStoreInventory();
     formatStoreActivity();
 
     let observer = new MutationObserver(bindClickPriceUndercut);
     observer.observe(document.body, {childList: true, subtree: true});
 }
 
+function sortStoreInventory() {
+    let tbody = document.evaluate("//table[@id='inv']/tbody", document).iterateNext();
+    if (tbody) {
+        let rows = [];
+        let row = tbody.firstChild.nextSibling;
+        while (row) {
+            let nextRow = row.nextSibling;
+            rows.push(row);
+            tbody.removeChild(row);
+            row = nextRow;
+        }
+
+        let getPrice = row => parseInt(row.children[3].firstChild.innerText.replace(/,/g, ""));
+        rows.sort((a, b) => getPrice(a) - getPrice(b));
+
+        let rowNum = 0;
+        while (rows.length) {
+            let row = rows.pop();
+            row.setAttribute("data-after", rowNum);
+            tbody.appendChild(row);
+            rowNum += 1;
+        }
+    }
+}
+
 function formatStoreActivity() {
-    let headingNode = document.evaluate("//b[text()='Recent Store Activity (past 2 weeks)']", document).iterateNext();
-    if (headingNode) {
-        let activityNode = headingNode.nextSibling.nextSibling;
+    let activityHeading = document.evaluate("//b[text()='Recent Store Activity (past 2 weeks)']", document).iterateNext();
+    if (activityHeading) {
+        let activityNode = activityHeading.nextSibling.nextSibling;
         activityNode.innerHTML = activityNode.innerHTML.replaceAll(
             /\d{4,}(?= Meat)/g, str => `<span style="color: ${getPriceColor(true, str, 0)}">${parseInt(str).toLocaleString()}</span>`);
     }
