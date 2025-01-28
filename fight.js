b651d5bc224e2720daaf4fc73d3f1b40aedc485c
Dickson Wong <dicksonwong@gmail.com>
2025-01-28 01:16:29 -0800

    Scroll with shift held to change combat action hotbar

--- fight.js
+++ fight.js
@@ -1,4 +1,17 @@
 function handleFight() {
-    bindKey("-", () => document.getElementById("button11")?.click());
     addPriceToAdventureRewardItems();
+
+    bindKey("-", () => document.getElementById("button11")?.click());
+
+    document.addEventListener("wheel", e => {
+        if (!e.shiftKey) return;
+        e.preventDefault();
+
+        let topbar = document.getElementById("topbar");
+        let actionBarUpButton = document.evaluate(".//img[@class='updown' and contains(@src, '/tinyup.gif')]", topbar).iterateNext();
+        let actionBarDownButton = document.evaluate(".//img[@class='updown' and contains(@src, '/tinydown.gif')]", topbar).iterateNext();
+
+        if (e.deltaY < 0) actionBarUpButton.click();
+        if (e.deltaY > 0) actionBarDownButton.click();
+    });
 }
