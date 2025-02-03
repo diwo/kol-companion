65a0995ee935c8f89113e4e74da6f7b968b4b8f1
Dickson Wong <dicksonwong@gmail.com>
2025-02-03 14:08:11 -0800

    Move quest close button to the left

--- charpane.js
+++ charpane.js
@@ -1,6 +1,7 @@
 function handleCharPane() {
     updateHpColor();
     updateMpColor();
+    moveQuestCloseButton();
 }
 
 function updateHpColor() {
@@ -14,5 +15,17 @@ function updateMpColor() {
         getMpTextNode().style.color = "green";
     } else if (isMpLow()) {
         getMpTextNode().style.color = "red";
+    }
+}
+
+function moveQuestCloseButton() {
+    let localSheets = Array.from(document.styleSheets).filter(s => s.href == null);
+    for (let sheet of localSheets) {
+        for (let rule of sheet.cssRules) {
+            if (rule.selectorText == "#nudges div .close") {
+                rule.style.right = null;
+                rule.style.left = "-10px";
+            }
+        }
     }
 }
