904d96064a9f6021ad226d968aa59503974ebbff
Dickson Wong <dicksonwong@gmail.com>
2025-11-10 13:11:08 -0800

    Hide existing /who when right clicking chat tab

--- mchat.js
+++ mchat.js
@@ -9,10 +9,30 @@ function chatBindActiveTabRightClick() {
         while (target) {
             if (target.classList?.contains("tab") && target.classList?.contains("active")) {
                 event.preventDefault();
-                sendCommand("/who");
+                if (!chatHideWholist()) {
+                    sendCommand("/who");
+                }
                 break;
             }
             target = target.parentNode;
         }
     });
+}
+
+function chatHideWholist() {
+    let allChats = document.getElementsByClassName("chatdisplay");
+    let currentChat = Array.from(allChats).filter(x => x.style.display != "none")[0];
+
+    let isAboveLastmark = false;
+    let hasWholistAfterLastmark = false;
+    for (let node=currentChat.lastChild; node; node=node.previousSibling) {
+        let isLastmark = node.className == "lastmark";
+        let isWholist = !!document.evaluate(".//center/b[starts-with(text(), 'Players in channel ')]", node).iterateNext();
+        let isVisible = node.style.display != "none";
+        if (isLastmark) isAboveLastmark = true;
+        if (isWholist && !isAboveLastmark && isVisible) hasWholistAfterLastmark = true;
+        if (isWholist) node.style.display = "none";
+    }
+
+    return hasWholistAfterLastmark;
 }
